9d8137b7b326ddc4d4338de28b8fc798f2d1ea5e
Taerogrammer <hyung54321@naver.com>
2023-10-31 00:24:05 +0900

    CardFlip-SwiftUI 생성

---
+++ SwiftUI/CardFlip-SwiftUI/CardFlip-SwiftUI/CardFlip_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CardFlip_SwiftUIApp.swift
+//  CardFlip-SwiftUI
+//
+//  Created by 김태형 on 2023/10/30.
+//
+
+import SwiftUI
+
+@main
+struct CardFlip_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
